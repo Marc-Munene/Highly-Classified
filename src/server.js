c63804d43900d1441a1c8acabaf47ccedcbdf137
Marc Munene <marcmunene104@gmail.com>
2025-03-11 14:21:35 +0300

    users

--- src/server.js
+++ src/server.js
@@ -4,6 +4,38 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+//home route
+app.get("/", (req, res) => {
+  res.json({
+    message: "This is the home page",
+  });
+});
+
+//users route
+app.get("/users", (req, res) => {
+  res.json({
+    message: "getting users",
+  });
+});
+
+app.post("/", (req, res) => {
+  res.json({
+    message: "adding users",
+  });
+});
+
+app.put("/", (req, res) => {
+  res.json({
+    message: "editing users",
+  });
+});
+
+app.delete("/", (req, res) => {
+  res.json({
+    message: "deleting users",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
