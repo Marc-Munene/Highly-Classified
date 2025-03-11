5ca32c1773c42a446030e6e070b6428483b70f2a
Marc Munene <marcmunene104@gmail.com>
2025-03-11 16:08:53 +0300

    Models

---
+++ src/database/models/user.js
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  regNo: { type: String, unique: true },
+  course: { type: String },
+  role: { type: String, enum: ["student", "lecturer", "classrep"] },
+});
+
+const User = new model("user", userSchema);
+
+export { User };
