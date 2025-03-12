d44c33bbca62572a8288b856916d91d91b83f97c
Marc Munene <marcmunene104@gmail.com>
2025-03-12 16:15:24 +0300

    user controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,8 +1,9 @@
-import { User } from "../database/models/user";
+import { User } from "../database/models/user.js";
 
+//Getting all users
 export const getUsers = async (req, res) => {
   try {
-    const users = User.find();
+    const users = await User.find();
 
     res.status(200).json({
       success: true,
@@ -17,20 +18,83 @@ export const getUsers = async (req, res) => {
   }
 };
 
-export const addUsers = (req, res) => {
-  res.json({
-    message: "adding users",
-  });
+//get one user
+export const getOneUser = async (req, res) => {
+  try {
+    const oneUser = await User.find({
+      name: { $regex: req.query.title, $options: "i" },
+    });
+
+    res.status(200).json({
+      success: true,
+      data: oneUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Cannot find user!",
+    });
+  }
 };
 
-export const editUsers = (req, res) => {
-  res.json({
-    message: "editing users",
-  });
+//add users
+export const addUsers = async (req, res) => {
+  const { name, email, regNo, role } = req.body;
+  try {
+    const userData = { name, email, regNo, role };
+    const newUser = await User.create(userData);
+
+    res.status(200).json({
+      success: true,
+      data: newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Faled!",
+    });
+  }
 };
 
-export const deleteUsers = (req, res) => {
-  res.json({
-    message: "deleting users",
-  });
+export const editUsers = async (req, res) => {
+  try {
+    const userId = req.query.userId;
+
+    const user = await User.findOneAndUpdate({ _id: userId }, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      succes: false,
+      message: "Faled!",
+    });
+  }
+};
+
+export const deleteUsers = async (req, res) => {
+  try {
+    const userId = req.query.id;
+
+    const deletedUser = await User.deleteOne({ _id: userId });
+
+    res.status(200).json({
+      succes: true,
+      deletedCount: deletedUser.deletedCount,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      succes: false,
+      message: "Failed!",
+    });
+  }
 };
--- src/database/models/class.js
+++ src/database/models/class.js
@@ -5,6 +5,11 @@ const classSchema = new Schema(
     building: { type: Schema.Types.ObjectId, ref: "building" },
     name: { type: String, required: true },
     capacity: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["Vacant", "Occupied", "Booked"],
+      default: "Vacant",
+    },
   },
   {
     timestamps: true,
