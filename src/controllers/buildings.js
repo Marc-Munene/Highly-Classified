9385edb41f3c122e93668387d276169f6926ff66
Marc Munene <marcmunene104@gmail.com>
2025-03-25 13:47:59 +0300

    controllers

--- src/controllers/buildings.js
+++ src/controllers/buildings.js
@@ -1,12 +1,35 @@
+import { Building } from "../database/models/building.js";
+
 export const getBuildings = (req, res) => {
   res.json({
     message: "getting buildings",
   });
 };
-export const addBuildings = (req, res) => {
-  res.json({
-    message: "adding buildings",
-  });
+export const addBuildings = async (req, res) => {
+  try {
+    const { name, location, contactInfo } = req.body;
+
+    if (!name || !location || !contactInfo) {
+      return res.status(409).json({
+        message: "All fields are required",
+      });
+    }
+    
+    const buildingData = { name, location, contactInfo };
+
+    const newBuilding = await Building.create(buildingData);
+
+    res.status(200).json({
+      success: true,
+      data: newBuilding,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed!",
+    });
+  }
 };
 export const editBuildings = (req, res) => {
   res.json({
--- src/controllers/classes.js
+++ src/controllers/classes.js
@@ -1,18 +1,61 @@
-export const getClasses = (req, res) => {
-  res.json({
-    message: "getting classes",
-  });
+import { Building } from "../database/models/building.js";
+import { Class } from "../database/models/class.js";
+
+export const getClasses = async (req, res) => {
+  try {
+    const classes = await Class.find().populate("building");
+
+    res.status(200).json({
+      success: true,
+      data: classes,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Faled to get classes!",
+    });
+  }
 };
-export const addClasses = (req, res) => {
-  res.json({
-    message: "adding classes",
-  });
+
+export const addClasses = async (req, res) => {
+  try {
+    const { building, name, capacity, status } = req.body;
+
+    if (!name || !capacity) {
+      return res.status(409).json({
+        message: "Name and Capacity are required",
+      });
+    }
+    const classData = { name, capacity, status };
+
+    const newClass = await Class.create(classData);
+
+    const existingBuilding = await Building.findById(building);
+
+    newClass.building = existingBuilding._id;
+
+    await newClass.save();
+
+    res.status(200).json({
+      success: true,
+      data: newClass,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Faled!",
+    });
+  }
 };
+
 export const editClasses = (req, res) => {
   res.json({
     message: "editing classes",
   });
 };
+
 export const deleteClasses = (req, res) => {
   res.json({
     message: "deleting classes",
